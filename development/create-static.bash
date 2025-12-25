3b9ccca010f314687062dfc71065dd1c7c6aac39
Udo Schlegel <udo.schlegel@lmu.de>
2025-12-25 16:40:00 +0100

    New version

--- development/create-static.bash
+++ development/create-static.bash
@@ -1,7 +1,12 @@
 
 docker build -t development .
-docker run --rm -v $(pwd)/output:/app/build development npm run build
+docker run --rm -v $(pwd)/output:/app/build development sh -c "npx update-browserslist-db@latest && npm run build"
 
-mkdir -p ../old
-find .. -mindepth 1 -maxdepth 1 ! -name '.gitignore' ! -name '.gitattributes' ! -name 'development' -exec mv {} ../old/ \;
-mv output/* ../.
+# Remove old files/directories (except .git, .gitignore, .gitattributes, and development)
+find .. -mindepth 1 -maxdepth 1 ! -name '.gitignore' ! -name '.gitattributes' ! -name 'development' ! -name '.git' -exec rm -rf {} +
+
+# Copy new build files to parent directory
+cp -rf output/* ../.
+
+# Clean up output directory
+rm -rf output
